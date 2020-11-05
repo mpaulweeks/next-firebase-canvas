ffc3cc84371cf57fc88287ef7813d1fbdea1c0d3
mpaulweeks <mpaulweeks@gmail.com>
2020-11-05 17:55:53 -0500

    refactor into classes with strong SRP

--- canvas.js
+++ canvas.js
@@ -1,82 +1,107 @@
 
-// setup canvas
-let myCanvas = document.getElementById('my-canvas');
-myCanvas.width = 600;
-myCanvas.height = 400;
-let ctx = myCanvas.getContext('2d');
+class DrawingBoard {
+  constructor() {
+    // setup canvas
+    this.canvas = document.getElementById('my-canvas');
+    this.canvas.width = 600;
+    this.canvas.height = 400;
+    this.ctx = this.canvas.getContext('2d');
 
-// helper functions
-function getPoint(event) {
-  let rect = event.target.getBoundingClientRect();
-  let x = event.clientX - rect.left; // x position within the element
-  let y = event.clientY - rect.top;  // y position within the element
-  return {x, y};
+    this.setupMouse();
+    this.setupErase();
+  }
+
+  // helper functions
+  getPoint(event) {
+    let rect = event.target.getBoundingClientRect();
+    let x = event.clientX - rect.left; // x position within the element
+    let y = event.clientY - rect.top;  // y position within the element
+    return {x, y};
+  }
+
+  setupMouse() {
+    // keep track of mouse dragging
+    let mouseDown = false;
+    let lastPoint = undefined;
+    document.body.addEventListener('mousedown', (event) => {
+      mouseDown = true;
+      lastPoint = this.getPoint(event);
+    });
+    document.body.addEventListener('mouseup', () => {
+      mouseDown = false;
+      lastPoint = undefined;
+    });
+
+    // draw lines between mouse movements
+    this.canvas.addEventListener('mousemove', (event) => {
+      if (mouseDown) {
+        let currPoint = this.getPoint(event);
+        if (lastPoint) {
+          this.ctx.strokeStyle = 'black';
+          this.ctx.beginPath();
+          this.ctx.moveTo(lastPoint.x, lastPoint.y);
+          this.ctx.lineTo(currPoint.x, currPoint.y);
+          this.ctx.closePath();
+          this.ctx.stroke();
+        }
+        lastPoint = currPoint;
+      }
+    });
+  }
+
+  setupErase() {
+    // listen for erase button
+    document.getElementById('clear').addEventListener('click', () => {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    });
+  }
+
+  toImageData() {
+    return this.canvas.toDataURL();
+  }
 }
-function canvasToData(canvas) {
-  return canvas.toDataURL();
-}
-function dataToCanvas(canvas, dataUrl) {
-  let ctx = canvas.getContext('2d');
-  let img = new Image();
-  img.onload = () => {
-    ctx.drawImage(img, 0, 0);
-  };
-  img.src = dataUrl;
-}
 
-// keep track of mouse dragging
-let mouseDown = false;
-let lastPoint = undefined;
-document.body.addEventListener('mousedown', (event) => {
-  mouseDown = true;
-  lastPoint = getPoint(event);
-});
-document.body.addEventListener('mouseup', () => {
-  mouseDown = false;
-  lastPoint = undefined;
-});
-
-// draw lines between mouse movements
-myCanvas.addEventListener('mousemove', (event) => {
-  if (mouseDown) {
-    let currPoint = getPoint(event);
-    if (lastPoint) {
-      ctx.strokeStyle = 'black';
-      ctx.beginPath();
-      ctx.moveTo(lastPoint.x, lastPoint.y);
-      ctx.lineTo(currPoint.x, currPoint.y);
-      ctx.closePath();
-      ctx.stroke();
-
-      // todo test
-      // updateRemoteCanvas('remote', canvasToData(myCanvas));
+class CanvasManager {
+  constructor() {
+    this.myId = Math.floor(Math.random() * 1000000);
+    this.myCanvas = new DrawingBoard();
+    this.otherCanvases = {};
+  }
+  getMyData() {
+    return {
+      id: this.myId,
+      image: this.myCanvas.toImageData(),
     }
-    lastPoint = currPoint;
   }
-});
-
-// listen for erase button
-document.getElementById('clear').addEventListener('click', () => {
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
-});
-
-// networking
-let otherCanvases = {};
-function updateRemoteCanvas(id, data) {
-  let canvas = otherCanvases[id];
-  if (!canvas) {
-    canvas = document.createElement('canvas');
-    canvas.width = 600;
-    canvas.height = 400;
-    document.body.appendChild(canvas);
-    otherCanvases[id] = canvas;
+  updateRemoteCanvas(data) {
+    let { id, image } = data;
+    if (id === this.myId) {
+      return;
+    }
+    let canvas = this.otherCanvases[id];
+    if (!canvas) {
+      canvas = document.createElement('canvas');
+      canvas.width = 600;
+      canvas.height = 400;
+      document.body.appendChild(canvas);
+      this.otherCanvases[id] = canvas;
+    }
+    let ctx = canvas.getContext('2d');
+    let imgObj = new Image();
+    imgObj.onload = () => {
+      ctx.drawImage(imgObj, 0, 0);
+    };
+    imgObj.src = image;
   }
-  dataToCanvas(canvas, data);
-}
-function removeRemoteCanvas(id) {
-  let canvas = otherCanvases[id];
-  if (canvas) {
-    canvas.remove();
+  removeRemoteCanvas(data) {
+    let { id } = data;
+    if (id === this.myId) {
+      return;
+    }
+    let canvas = this.otherCanvases[id];
+    if (canvas) {
+      canvas.remove();
+    }
   }
 }
--- network.js
+++ network.js
@@ -1,42 +1,34 @@
-let myId = Math.floor(Math.random() * 1000000);
+let manager = new CanvasManager();
 
-let myRef = firebase.database().ref('user/' + myId);
+let myRef = firebase.database().ref('user/' + manager.myId);
+let userRef = firebase.database().ref('user');
+
 // safely cleanup canvas after closing
 myRef.onDisconnect().remove();
+
 // update remove database every 3 seconds
 setInterval(() => {
-  myRef.set({
-    id: myId,
-    data: canvasToData(myCanvas),
-  });
+  myRef.set(manager.getMyData());
 }, 3000);
 
-let userRef = firebase.database().ref('user');
 // read all other canvases once on pageload
 userRef.once('value', (snapshot) => {
   let updates = Object.values(snapshot.val());
   updates.forEach((update) => {
-    if (update.id !== myId) {
-      updateRemoteCanvas(update.id, update.data);
-    }
+    manager.updateRemoteCanvas(update);
   });
 });
+
 // read when canvases are added/edited/removed
 userRef.on('child_added', (snapshot) => {
   let update = snapshot.val();
-  if (update.id !== myId) {
-    updateRemoteCanvas(update.id, update.data);
-  }
+  manager.updateRemoteCanvas(update);
 });
 userRef.on('child_changed', (snapshot) => {
   let update = snapshot.val();
-  if (update.id !== myId) {
-    updateRemoteCanvas(update.id, update.data);
-  }
+  manager.updateRemoteCanvas(update);
 });
 userRef.on('child_removed', (snapshot) => {
   let update = snapshot.val();
-  if (update.id !== myId) {
-    removeRemoteCanvas(update.id);
-  }
+  manager.removeRemoteCanvas(update);
 });
